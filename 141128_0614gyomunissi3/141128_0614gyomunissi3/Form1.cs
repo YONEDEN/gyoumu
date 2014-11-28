b2e712d3b4917060163193995a977e2fdf2a8663
JYL <jyl2142@gmail.com>
2014-11-28 20:39:31 +0900

    中途半端なテキストエディタ

--- 141128_0614gyomunissi3/141128_0614gyomunissi3/Form1.cs
+++ 141128_0614gyomunissi3/141128_0614gyomunissi3/Form1.cs
@@ -9,8 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms; 
 using System.IO;
 
-//Version0.1　Gitテスト用3
-
+//Version0.1　中途半端なテキストエディタ
 
 namespace _141128_0614gyomunissi3
 {
@@ -20,8 +19,12 @@ namespace _141128_0614gyomunissi3
         //初期設定
         {
             InitializeComponent();
-            this.Text = "業務日誌";
+            this.Text = "業務日誌基礎試験プログラム";
             this.button1.Text = "消去";
+            this.button2.Text = "読み込み";
+            this.button3.Text = "上書き";
+            this.label1.Text = "入力したものがここに表示されます";
+            this.label4.Text = "I/O";
             //時計起動
             timer1.Start();
         }
@@ -30,37 +33,37 @@ namespace _141128_0614gyomunissi3
         private void button1_Click(object sender, EventArgs e)
         //消去ボタン動作        
         {
-        //    this.Text = textBox1.Text;
-        //    label1.Text = textBox1.Text;
-        //    MessageBox.Show(textBox1.Text, textBox1.Text);
-        DialogResult result = MessageBox.Show(textBox1.Text + "消去しますか？",
-            "質問",
-            MessageBoxButtons.YesNoCancel,
-            MessageBoxIcon.Question,
-            MessageBoxDefaultButton.Button2);
+            //    this.Text = textBox1.Text;
+            //    label1.Text = textBox1.Text;
+            //    MessageBox.Show(textBox1.Text, textBox1.Text);
+            DialogResult result = MessageBox.Show(textBox1.Text + "消去しますか？",
+                "質問",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button2);
 
-        //何が選択されたか調べる
+            //何が選択されたか調べる
             if (result == DialogResult.Yes)
-                {
-            //「はい」が選択された時
-            Console.WriteLine("「はい」が選択されました");
-            label1.Text = "";
-            textBox1.Text = "";
-                }
+            {
+                //「はい」が選択された時
+                Console.WriteLine("「はい」が選択されました");
+                label1.Text = "";
+                textBox1.Text = "";
+            }
             else if (result == DialogResult.No)
-                {
-            //「いいえ」が選択された時
-            Console.WriteLine("「いいえ」が選択されました");
-            textBox1.Text = textBox1.Text;
-                }
+            {
+                //「いいえ」が選択された時
+                Console.WriteLine("「いいえ」が選択されました");
+                textBox1.Text = textBox1.Text;
+            }
             else if (result == DialogResult.Cancel)
-                {
-            //「キャンセル」が選択された時
-            Console.WriteLine("「キャンセル」が選択されました");
-                }
+            {
+                //「キャンセル」が選択された時
+                Console.WriteLine("「キャンセル」が選択されました");
+            }
         }
-            
-            
+
+
         private void timer1_Tick(object sender, EventArgs e)
         //時計表示
         {
@@ -70,27 +73,38 @@ namespace _141128_0614gyomunissi3
             label3.Text = datetime.ToLongTimeString();
 
         }
-
-        private void textBox1_TextChanged_1(object sender, EventArgs e)
-        //ラベルににテキストボックス転記
+      
+        private void button2_Click(object sender, System.EventArgs e)
+        //ファイル読み込みボタン動作
         {
-            label1.Text = textBox1.Text;
+        Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
+        StreamReader sr =
+                    new StreamReader("Test.txt",sjisEnc);
+        String s = sr.ReadToEnd();
+        sr.Close();
+        MessageBox.Show(s);
+        textBox1.Text = s;
         }
-        class TestIO
-        //ファイルI/O試験
-        {
-            static void sub()
-            {
-                if (!Directory.Exists("test"))
-                    Directory.CreateDirectory("test");
 
-                for (int i = 0; i < 5; ++i)
-                {
-                    string fileName = string.Format(@"test\{0}.txt", i);
-                    string contents = string.Format("Test file No. {0}", i);
-                    File.WriteAllText(fileName, contents);
-                }
-            }
-}
-  }
+ 
+
+        private void button3_Click(object sender, System.EventArgs e)
+        //フィイル出力ボタン操作
+        {
+        Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
+        StreamWriter writer =
+                    new StreamWriter("Test.txt", true, sjisEnc);
+        writer.Write(textBox1.Text);
+        writer.Close();
+        }
+
+           
+                
+          
+
+//        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+//        {
+
+//        }
+    }
 }
