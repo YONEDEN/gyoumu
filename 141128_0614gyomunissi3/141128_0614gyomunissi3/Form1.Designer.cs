a566049a607e1f4056dbda828593c3b355ccf550
JYL <jyl2142@gmail.com>
2014-11-29 20:18:49 +0900

    機動機からの変更

---
+++ 141128_0614gyomunissi3/141128_0614gyomunissi3/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace _141128_0614gyomunissi3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 462);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.textBox1.Location = new System.Drawing.Point(45, 118);
+            this.textBox1.MaximumSize = new System.Drawing.Size(520, 400);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(520, 400);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(29, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(29, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(599, 418);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(599, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(599, 307);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(246, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(599, 29);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 544);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(246, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(246, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "label6";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(599, 79);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(700, 565);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ 141128_0614gyomunissi3/141128_0614gyomunissi3/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; 
+using System.IO;
+using System.Text.RegularExpressions ;  // for Regex
+
+//Version0.1　中途半端な業務日誌用テキストエディタ Git同期テスト１　端末chiakiから（２）
+
+namespace _141128_0614gyomunissi3
+{
+    public partial class Form1 : Form
+        //フォームクラス
+        {
+        public Form1()
+        //初期設定
+        {
+            InitializeComponent();
+
+            this.Text = "業務日誌基礎試験プログラム";
+            this.button1.Text = "消去";
+            this.button2.Text = "読み込み";
+            this.button3.Text = "上書き";
+            this.button4.Text = "ファイル消去";
+            this.button5.Text = "終了";
+            //時計起動
+            timer1.Start();
+            filecheck();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        //消去ボタン動作        
+        {
+            DialogResult result = MessageBox.Show("フォーム内を消去しますか？",
+                "質問",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button2);
+
+            //何が選択されたか調べる
+            if (result == DialogResult.Yes)
+            {
+                //「はい」が選択された時
+                textBox1.Text = "";
+            }
+            else if (result == DialogResult.No)
+            {
+                //「いいえ」が選択された時
+                textBox1.Text = textBox1.Text;
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        //時計表示
+        {
+            DateTime datetime = DateTime.Now;
+            string Wlet = ("日月火水木金土").Substring(
+                        int.Parse(DateTime.Now.DayOfWeek.ToString("d")),
+                        1); 
+
+            label2.Text = datetime.ToLongDateString() + "（" + Wlet + "）";//ラベル２は日付
+            label3.Text = datetime.ToLongTimeString();//ラベル３は時刻
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        //ファイル読み込みボタン動作
+        {
+            //String load_fn = "";
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "プロジェクトファイルを開く";
+            ofd.InitialDirectory = "";  // 最初に表示されるディレクトリ
+            ofd.Filter = "datファイル(*.dat)|*.dat|すべてのファイル(*.*)|*.*";  //「ファイルの種類」を指定
+            ofd.CheckFileExists = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                label4.Text = ofd.FileName; System.IO.Stream stream;
+                stream = ofd.OpenFile();
+                if (stream != null)
+                {
+                    //内容を読み込み、表示する
+                    //Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
+                    System.IO.StreamReader sr =
+                           new System.IO.StreamReader(stream,
+                           System.Text.Encoding.GetEncoding("Shift_JIS"));
+                    String s = sr.ReadToEnd();
+                    sr.Close();
+                    MessageBox.Show("読み込みます");
+                    textBox1.Text = s;
+                    filecheck();
+                    //Console.WriteLine(sr.ReadToEnd());
+                    //閉じる
+                    sr.Close();
+                    stream.Close();
+                    //textBox1.Text = ofd.FileName;
+                }
+            }
+
+        }
+        private void button3_Click(object sender, System.EventArgs e)
+        //ファイル出力ボタン操作
+        {
+            Encoding sjisEnc = Encoding.GetEncoding("Shift_JIS");
+            StreamWriter writer =
+                        new StreamWriter(name.f_name, false, sjisEnc);
+            writer.Write(textBox1.Text + name.dcode + DateTime.Now.ToString());
+            writer.Close();
+            MessageBox.Show("書き込みました");
+            filecheck();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        //全消去ボタン動作        
+        {
+            //string f_name = "test.csv";
+            DialogResult result = MessageBox.Show("作業ファイルを消去しますか？",
+                "質問",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button2);
+
+            //何が選択されたか調べる
+            if (result == DialogResult.Yes)
+            {
+                //「はい」が選択された時
+                FileStream fs = File.Create(name.f_name);
+                fs.Close();
+                textBox1.Text = "";
+
+            }
+            else if (result == DialogResult.No)
+            {
+                //「いいえ」が選択された時
+                textBox1.Text = textBox1.Text;
+            }
+            else if (result == DialogResult.Cancel)
+            {
+                //「キャンセル」が選択された時
+            }
+            filecheck();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void filecheck()
+        //ファイル最終更新時刻取得
+        {
+            DateTime fct, flat, flwt;
+            try
+            {
+                fct = File.GetCreationTime(label4.Text);
+                flat = File.GetLastAccessTime(label4.Text);
+                flwt = File.GetLastWriteTime(label4.Text);
+            }
+            catch
+            {
+                label1.Text = "ファイル名がＮＧです！";
+                return;
+            }
+            label1.Text = "作成日　 = " + fct.ToString();
+            label5.Text = "最終閲覧 = " + flat.ToString();
+            label6.Text = "最終更新 = " + flwt.ToString();
+
+        }
+
+   
+    }
+    class name
+        //作業用ファイル名設定(クラス)
+    {
+        public static string f_name = "testdata.dat";//入出力ファイル
+        //public static string kaigyo = "\r\n";//改行文字用固定変数
+        public static string dcode = "[maketime]";//改行文字用固定変数
+    }
+
+            
+
+}
